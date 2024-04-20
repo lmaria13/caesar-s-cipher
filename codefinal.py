02f3473b94adc7198c05f3779728f9e1ce2c8467
Maria <146199074+lmaria13@users.noreply.github.com>
2024-04-20 16:45:32 +0300

    Update codefinal.py

---
+++ codefinal.py
@@ -0,0 +1,26 @@
+print("Давай я зашифрую твоё сообщение по технике Цезаря.")
+alphabet = 'абвгдеёжзиклмнопрстуфхцчшщъыьэюяабвгдеёжзиклмнопрстуфхцчшщъыьэюя' # алфавит введён в список два раза, так как при смещении на большой шаг алфавит начинается сначала
+while True:
+    message = (input("Напиши слово кириллицей.\n")).lower() # возводим всё сообщение в нижний регистр для проверки по списку
+    for check in alphabet:
+        if message.lower().find(check.lower()):
+            break
+        else:
+            print("Я не знаю такого алфавита.") # защита от введения неверной раскладки
+cypher = ''
+print("Теперь надо придумать ключ.")
+while True:
+    key = int(input("Назови любое число от 1 до 32.\n"))
+    if 0 < key < 33:
+        break
+    else:
+        print("Такой ключ не подойдёт.") # защита от введения неверного ключа
+for step in message:
+    order = alphabet.find(step)
+    new_order = order + key
+    if step in alphabet:
+        cypher += alphabet[new_order]
+    else:
+        cypher += step
+answer = cypher.capitalize()
+print(f"Зашифрованное послание — {answer}.")
